68cbe17d0d9fd9d3e9d77250274aaa2c7e1855c2
vickyylopezz <vlopez@fi.uba.ar>
2023-04-18 16:41:59 -0300

    fix: server hace upload de archivos ok

--- SelectiveRepeat.py
+++ SelectiveRepeat.py
@@ -4,7 +4,7 @@ class SelectiveRepeat:
 
     WINDOW_SIZE = 4
     TIMEOUT = 2
-    CHUNK_SIZE = 1471
+    CHUNK_SIZE = 1470
     SEQ_NUM_SIZE = 2
 
     def __init__(self, socket):
@@ -29,19 +29,16 @@ class SelectiveRepeat:
         # Send packets to server
         while True:
             chunk = file.read(self.CHUNK_SIZE)
-            print(len(packets))
-            packets.append(chunk)
             if not chunk:
                 break
-        
+            packets.append(chunk)
         while True:
             # Send packets up to window size
             while self.next_seq_num < self.base + self.WINDOW_SIZE and self.next_seq_num < len(packets):
-                print(packets)
-                print(self.next_seq_num)
                 packet = packets[self.next_seq_num]
                 seq_num = self.next_seq_num
-                self.socket.sendto(self.__pack(seq_num.to_bytes(2, byteorder="big"), packet), addr)
+                data = self.__pack(seq_num.to_bytes(2, byteorder="big"), packet)
+                self.socket.sendto(data, addr)
                 self.unacked_packets[seq_num] = packet
                 self.next_seq_num += 1
 
@@ -49,9 +46,7 @@ class SelectiveRepeat:
             #self.socket.timeout(TIMEOUT)
             try:
                 ack, addr = self.socket.receive()
-                print("recibi ack")
                 seq_num_receive, _ = self.__unpack(ack)
-                print(seq_num_receive)
                 ack_num = int.from_bytes(seq_num_receive, "big")
                 if ack_num >= self.base:
                     # Update base and remove acknowledged packets from unacked_packets
@@ -69,37 +64,36 @@ class SelectiveRepeat:
                 break
 
     def receive(self, file, buffsize):
-        file.write(b'hola2')
-        while True:
+        rcv_data = 0
+        while rcv_data < buffsize:
             # Receive packet
             packet, addr = self.socket.receive()
             seq_num, data = self.__unpack(packet)
             seq_num_int = int.from_bytes(seq_num, "big")
             #seq_num = self.expected_seq_num
-            print("recibi paquete: ", packet)
-                
+            
             # Send ACK for received packet
             ack = self.__pack(seq_num, data)
             self.socket.sendto(ack, addr)
-            print("mande ack")
-
+            #print("mande ack")
 
             # If the packet is the expected one, write its data to the file
             if  seq_num_int == self.expected_seq_num:
                 # Write packet data to file
-                write = file.write(data)
-                print("escribi archivo",file, "con: ", data, write)
+                file.write(data)
                 self.expected_seq_num += 1
-                
-                print("Buffer:", self.buffer)
-                print("expected seq num:", self.expected_seq_num)
-                print(self.expected_seq_num in self.buffer)
+                rcv_data += len(data)
+
+                #print("Buffer:", self.buffer)
+                #print("expected seq num:", self.expected_seq_num)
                 
                 # Write any consecutive packets that were received out of order
                 while self.expected_seq_num in self.buffer:
-                    print("expected seq num:", self.expected_seq_num)
+                    #print("expected seq num:", self.expected_seq_num)
                     file.write(self.buffer[self.expected_seq_num])
                     del self.buffer[self.expected_seq_num]
                     self.expected_seq_num += 1
+                    rcv_data += len(self.buffer[self.expected_seq_num])+1
+
             else:
                 self.buffer[seq_num] = data
