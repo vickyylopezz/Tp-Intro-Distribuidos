021b1c3d9730ef64f9abf1b3e6d41179d549e6c3
vickyylopezz <vlopez@fi.uba.ar>
2023-04-22 17:59:59 -0300

    refactor: seq_numer de 3 bytes y lectura archivo

--- SelectiveRepeat.py
+++ SelectiveRepeat.py
@@ -1,11 +1,12 @@
 import socket
+import math
 
 class SelectiveRepeat:
 
     # WINDOW_SIZE = 4
     TIMEOUT = 2
-    CHUNK_SIZE = 1470
-    SEQ_NUM_SIZE = 2
+    CHUNK_SIZE = 1469
+    SEQ_NUM_SIZE = 3
 
     def __init__(self, socket):
         self.socket = socket
@@ -28,30 +29,30 @@ class SelectiveRepeat:
         packets = []
 
         # Send packets to server
-        while True:
-            
-            chunk = file.read(self.CHUNK_SIZE)
-            if not chunk:
-                break
-            packets.append(chunk)
+        # while True:
+        #     chunk = file.read(self.CHUNK_SIZE)
+        #     if not chunk:
+        #         break
+        #     packets.append(chunk)
 
-        self.window_size = (len(packets) / 2) - 1 if ((len(packets) / 2) - 1 > 0) else 1
-
+        len_packets = math.ceil(file.size() / self.CHUNK_SIZE)
+        self.window_size = (len_packets / 2) - 1 if ((len_packets / 2) - 1 > 0) else 1
         while True:
 
             # Send packets up to window size
-            while self.next_seq_num < self.base + self.window_size and self.next_seq_num < len(packets):
-                packet = packets[self.next_seq_num]
+            while self.next_seq_num < self.base + self.window_size and self.next_seq_num < len_packets:
+                #packet = packets[self.next_seq_num]
+                packet = file.read(self.CHUNK_SIZE)
                 seq_num = self.next_seq_num
-                data = self.__pack(seq_num.to_bytes(2, byteorder="big"), packet)
+                data = self.__pack(seq_num.to_bytes(3, byteorder="big"), packet)
                 self.socket.sendto(data, addr)
-                print("Envie paquete:", seq_num)
+                #print("Envie paquete:", seq_num)
                 self.unacked_packets[seq_num] = packet
                 self.next_seq_num += 1
             
             # Receive ACKs and update state
             self.socket.timeout(self.TIMEOUT)
-            if(self.base < len(packets)):
+            if(self.base < len_packets):
                 try:
                     ack, addr = self.socket.receive()
                     seq_num_receive, _ = self.__unpack(ack)
@@ -59,7 +60,7 @@ class SelectiveRepeat:
                     print("Recibi ack de:", ack_num, "y base:", self.base)
                     del self.unacked_packets[ack_num]
                     self.base += 1
-                    print("Elimine ack de:", ack_num)
+                    #print("Elimine ack de:", ack_num)
                     #if ack_num == self.base:
                     #    aux = self.base + 1
                     #    while aux not in self.unacked_packets and aux < len(packets):
@@ -74,12 +75,12 @@ class SelectiveRepeat:
                 except socket.timeout:
                     # Resend unacknowledged packets
                     for seq_num, packet in self.unacked_packets.items():
-                        print("Reenvie paquete:", seq_num)
-                        self.socket.sendto(self.__pack(seq_num.to_bytes(2, byteorder="big"), packet), addr)
+                        #print("Reenvie paquete:", seq_num)
+                        self.socket.sendto(self.__pack(seq_num.to_bytes(3, byteorder="big"), packet), addr)
 
             # Exit loop if all packets have been acknowledged
-            if self.base == len(packets):
-                print("entre if con len packets:", len(packets))
+            if self.base == len_packets:
+                #print("entre if con len packets:", len_packets)
                 break
 
 
@@ -91,13 +92,13 @@ class SelectiveRepeat:
         while True:
         #while rcv_data < buffsize:
             try:
-                print(rcv_data)
+                #print(rcv_data)
                 # Receive packet
                 packet, addr = self.socket.receive()
                 seq_num, data = self.__unpack(packet)
                 seq_num_int = int.from_bytes(seq_num, "big")
                 print("Recibi paquete numero:" ,seq_num_int, "y esperaba: ", self.expected_seq_num)
-                print("Ultimo escribi:", last_write)
+                #print("Ultimo escribi:", last_write)
                 #seq_num = self.expected_seq_num
                 
                 # Send ACK for received packet
@@ -118,23 +119,23 @@ class SelectiveRepeat:
                     #print("Buffer:", self.buffer)
                     #print("expected seq num:", self.expected_seq_num)
 
-                    print("last write:", last_write, "and expected:", self.expected_seq_num, "and i:", last_write+1)
-                    print(rcv_data)
+                    #print("last write:", last_write, "and expected:", self.expected_seq_num, "and i:", last_write+1)
+                    #print(rcv_data)
                     while rcv_data < buffsize:
                         try: 
                             data = self.buffer[last_write+1]
-                            print("Escribi:", self.expected_seq_num, last_write+1)
+                            #print("Escribi:", self.expected_seq_num, last_write+1)
                             #print("expected seq num:", self.expected_seq_num)
                             file.write(data)
                             del self.buffer[last_write+1]
                             self.expected_seq_num += 1
                             rcv_data += len(data)
                             last_write += 1
-                            print("las write:", last_write, "expected:", self.expected_seq_num)
+                            #print("las write:", last_write, "expected:", self.expected_seq_num)
                         except:
                             break
                 else:
-                    print("Escribi paquete:", seq_num_int, "en el buffer")
+                    #print("Escribi paquete:", seq_num_int, "en el buffer")
                     self.buffer[seq_num_int] = data
             except socket.timeout:
                 break
